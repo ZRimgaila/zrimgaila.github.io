355704e0033ca92c115c7e8e0a3d7e03823f8eca
ZRimgaila <zygis.rimgaila@gmail.com>
2025-05-05 12:00:58 +0300

    delete backend

--- my_portfolio/src/main/java/zr/spring_boot/my_portfolio/repository/ContactInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package zr.spring_boot.my_portfolio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import zr.spring_boot.my_portfolio.model.ContactInformation;
-
-public interface ContactInfoRepository extends JpaRepository<ContactInformation, String> {
-}
--- my_portfolio/src/main/java/zr/spring_boot/my_portfolio/repository/ProjectsRepository.java
+++
@@ -1,7 +0,0 @@
-package zr.spring_boot.my_portfolio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import zr.spring_boot.my_portfolio.model.Project;
-
-public interface ProjectsRepository extends JpaRepository<Project, Integer> {
-}
